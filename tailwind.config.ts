9c0834545c9c36b4b148cde912fe35c52414e925
hadi khir <khir.hadi@gmail.com>
2024-11-13 12:41:52 -0500

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,12 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif']
+      },
+      gridTemplateColumns: {
+        '70/30': '70% 28%'
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
